3483405aaa02bce1a75d17021f28f1595bdb5265
James D. && Mike S <james+mike@appacademy.io>
2015-08-13 10:49:05 -0400

    Add associations between User-Poll & User-Response.

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,11 @@
 class Response < ActiveRecord::Base
   validates :answer_choice_id, presence: true
   validates :respondent_id, presence: true
+
+  belongs_to(
+    :respondent,
+    :class_name => "User",
+    :foreign_key => :respondent_id,
+    :primary_key => :id
+  )
 end
