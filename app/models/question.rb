0131e8e89acad133bba75e17b78c3dd11beb4203
James D. && Mike S <james+mike@appacademy.io>
2015-08-13 13:37:07 -0400

    Add Question#results.

--- app/models/question.rb
+++ app/models/question.rb
@@ -22,4 +22,50 @@ class Question < ActiveRecord::Base
     :source => :responses
   )
 
+  def results
+    ####The bad way
+    # choices = self.answer_choices
+    # choice_count = {}
+    #
+    # choices.each do |choice|
+    #   choice_count[choice.answer_choice] = choice.responses.count
+    # end
+    #
+    # choice_count
+
+    ####Better way
+    # choices =  self.answer_choices.includes(:responses)
+    # choice_count = {}
+    #
+    # choices.each do |choice|
+    #   choice_count[choice.answer_choice] = choice.responses.length
+    # end
+    # choice_count
+
+    #####Best way
+    answer_choices = self.answer_choices
+      .joins("LEFT OUTER JOIN
+        responses ON answer_choices.id = responses.answer_choice_id")
+      .where("answer_choices.question_id = ?", self.id)
+      .group("answer_choices.id")
+      .select("answer_choices.*, COUNT(responses.id) AS response_count")
+
+    answer_choices.map do |choice|
+      [choice.answer_choice, choice.response_count]
+    end
+
+    # SELECT
+    #   answer_choices.*, COUNT(responses.id)
+    # FROM
+    #   answer_choices
+    # LEFT OUTER JOIN
+    #   responses ON answer_choices.id = responses.answer_choice_id
+    # WHERE
+    #   answer_choices.question_id = ?
+    # GROUP BY
+    #   answer_choices.id
+
+
+  end
+
 end
