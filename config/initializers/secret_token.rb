38f426ae55d424c6b003a7ad5f76f09b0c8d6a2f
James D. && Mike S <james+mike@appacademy.io>
2015-08-13 10:13:40 -0400

    Add User model & migration.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polls::Application.config.secret_key_base = 'aefce789ebce31650260a31e02dcb5897cd21255dca66d5d11b6d9170a44ad9a566b4cd5bfca6329160f277d126c00df9ad55cf6696cfc6f513c5ad2a8993645'
